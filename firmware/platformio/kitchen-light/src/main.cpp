a4b6b1eb54b5b1a9cf3f87c37186e3bea8c918ed
Peter Cizmar <pcizmar95@gmail.com>
2024-03-02 16:51:38 +0100

    removed psram support, added lib dependencies

--- firmware/platformio/kitchen-light/src/main.cpp
+++ firmware/platformio/kitchen-light/src/main.cpp
@@ -1,4 +1,15 @@
+// core
 #include <Arduino.h>
+
+// rotary encoder
+#include <RotaryEncoder.h>
+
+// display
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+
+// LED strip
+#include <FastLED.h>
 
 void setup() {
   // put your setup code here, to run once:
